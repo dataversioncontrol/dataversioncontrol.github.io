4de66fcc7135ae2e75829798d02a0329cfc603ef
Gleb Lozhkovoy <lozhkovoygleb@icloud.com>
2018-03-20 22:59:59 +0200

    Build updates

---
+++ .cache/async-requires.js
@@ -0,0 +1,29 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/src/pages/404.js"),
+  "component---src-pages-docs-js": require("gatsby-module-loader?name=component---src-pages-docs-js!/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/src/pages/docs.js"),
+  "component---src-pages-download-js": require("gatsby-module-loader?name=component---src-pages-download-js!/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/src/pages/download.js"),
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/src/pages/index.js"),
+  "component---src-pages-support-js": require("gatsby-module-loader?name=component---src-pages-support-js!/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/src/pages/support.js")
+}
+
+exports.json = {
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/.cache/json/layout-index.json"),
+  "404.json": require("gatsby-module-loader?name=path---404!/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/.cache/json/404.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/.cache/json/layout-index.json"),
+  "docs.json": require("gatsby-module-loader?name=path---docs!/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/.cache/json/docs.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/.cache/json/layout-index.json"),
+  "download.json": require("gatsby-module-loader?name=path---download!/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/.cache/json/download.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/.cache/json/layout-index.json"),
+  "index.json": require("gatsby-module-loader?name=path---index!/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/.cache/json/index.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/.cache/json/layout-index.json"),
+  "support.json": require("gatsby-module-loader?name=path---support!/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/.cache/json/support.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/.cache/json/layout-index.json"),
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/.cache/json/404-html.json")
+}
+
+exports.layouts = {
+  "layout---index": require("gatsby-module-loader?name=component---src-layouts-index-jsx!/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/.cache/layouts/index.js")
+}
---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/src/layouts/index.jsx"
+  import data from "/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
