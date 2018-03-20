8e03edcccf772ac46138f8dbbf7b9b9f13942bb1
Gleb Lozhkovoy <lozhkovoygleb@icloud.com>
2018-03-21 00:03:29 +0200

    Updates regrading feedback

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/src/layouts/index.jsx"
-  import data from "/Users/gleblozhkovoy/Work/dataversioncontrol.github.io/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
