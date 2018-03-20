ce6334820313dc291d4903af5055ec204926db0f
Gleb Lozhkovoy <lozhkovoygleb@icloud.com>
2018-03-20 14:49:10 +0200

    Release

--- docs/path---index-df09d4704a4c94f6fc86.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0x81b8806e4260],{359:function(t,o){t.exports={data:{site:{siteMetadata:{videoId:"X8oEcNhKrZM",githubUrl:"https://github.com/dataversioncontrol",downloadUrl:"/",twitter:"dataversioncontrol",linkedin:"dataversioncontrol",facebook:"dataversioncontrol"}}},pathContext:{}}}});
-//# sourceMappingURL=path---index-df09d4704a4c94f6fc86.js.map
